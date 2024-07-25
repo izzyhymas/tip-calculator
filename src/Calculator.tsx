ec4608a937ae043dc449e630b9ffaa68b5e95ba1
Izzy Hymas <izzyhymas@gmail.com>
2024-07-25 17:17:14 -0600

    Add function docstrings

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 
 import styles from "./Calculator.module.css";
 
+/**
+ *
+ * @returns {JSX.Element} The rendered Calculator comonent
+ */
 const Calculator: React.FC = () => {
   // State for storing bill
   const [bill, setBill] = useState<string>("");
@@ -10,23 +14,39 @@ const Calculator: React.FC = () => {
   // State for storing total bill after tip
   const [total, setTotal] = useState<number | null>(null);
 
-  // Checks if string is a valid number
+  /**
+   * Checks if string is a valid number
+   *
+   * @param {string} num - The string to check
+   * @returns {boolean} - True if string is a valid number, false otherwise
+   */
   const isNumber = (num: string): boolean => /^\d*\.?\d{0,2}$/.test(num);
 
-  // Prevents page from refreshing when the submit button is clicked
+  /**
+   * Handles form submission and calculates total after tip
+   * 
+   * @param {React.FormEvent<HTMLFormElement>} event - The form submission event 
+   */
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // Prevents page from refreshing when the submit button is clicked
     event.preventDefault();
 
     if (bill && typeof tipPercentage === "number") {
       const tipAmount = Number(bill) * (tipPercentage / 100);
       const totalAmount = Number(bill) + tipAmount;
-      const roundedTotal = Math.round(totalAmount * 100) / 100; 
+      const roundedTotal = Math.round(totalAmount * 100) / 100;
       setTotal(roundedTotal);
     } else {
       alert("Please enter valid numbers");
     }
   };
 
+  /**
+   * Handles changes to bill input
+   * 
+   * @param {React.ChangeEvent<HTMLInputElement>} event - Bill amount input change event 
+   */
   const handleBillChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ): void => {
